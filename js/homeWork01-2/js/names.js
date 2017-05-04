28c7eb7ba09657964a13fe11b4827c7f6bb0a8a4
d_ryzhkov <rd.pochta@gmail.com>
2017-05-05 00:33:15 +0300

    js homework1-02 : second task finished.

---
+++ js/homeWork01-2/js/names.js
@@ -0,0 +1,21 @@
+/**
+ * Created by ryzhkov on 04.05.17.
+ */
+var names = [];
+for (var i = 0; i < 5; i++) {
+    names[i] = prompt('Enter user names to DB ! \n' + (5 - i) + ' left.', "");
+}
+var userName = prompt('Enter user name!', '');
+nameCheck(userName, names);
+function nameCheck(name, dataBase) {
+    var isCorrect = false;
+    for (var k = 0; k < dataBase.length; k++) {
+        if (name === dataBase[k]) {
+            isCorrect = true;
+            var message = 'Hello, ' + name;
+            alert(message);
+            break;
+        }
+    } if (!isCorrect) alert('No such username!')
+}
+console.log(names);
