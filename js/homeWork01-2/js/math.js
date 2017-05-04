4c4604ce3989a92ebdf218abda8adf42490535aa
d_ryzhkov <rd.pochta@gmail.com>
2017-05-04 23:17:55 +0300

    js homework1-02 code refactoring done.

--- js/homeWork01-2/js/math.js
+++ js/homeWork01-2/js/math.js
@@ -2,22 +2,23 @@
  * Created by ryzhkov on 04.05.17.
  */
 function pow(number, rate) {
-    var result = number;
+    var result = 1;
     if (number !== 0 && rate > 0) {
-        for (var i = 0; i < rate - 1; i++) {
+        for (var i = 0; i < rate; i++) {
             result *= number;
         }
     }
     else if (number !== 0 && rate < 0) {
-        var temp = number;
-        for (var k = 0; k < (rate * -1) -1 ; k++) {
+        var temp = 1;
+        for (var k = 0; k < rate * -1; k++) {
             temp *= number;
-        } result = 1 / temp;
+        }
+        result = 1 / temp;
     }
     else if (number === 0) {
         result = 0;
     }
-    else if (number !== 0 && rate === 0) {
+    else if (rate === 0) {
         result = 1;
     }
     return result;
