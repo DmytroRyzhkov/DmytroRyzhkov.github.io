1bab623c276cca60cd003695cf313207113dd182
d_ryzhkov <rd.pochta@gmail.com>
2017-05-05 16:34:16 +0300

    js homework1-02 : names task code reformatting done .

--- js/homeWork01-2/task2/js/names.js
+++ js/homeWork01-2/task2/js/names.js
@@ -3,11 +3,20 @@
  */
 "use strict";
 var names = [];
+var name;
 for (var i = 0; i < 5; i++) {
-    names[i] = prompt('Enter user names to DB ! \n' + (5 - i) + ' left.', "");
+    do {
+        name = prompt('Enter user names to DB ! \n' + (5 - i) + ' left.', '');
+        names[i] = name;
+    }
+    while (!isValid(name));
 }
 var userName = prompt('Enter user name!', '');
-nameCheck(userName, names);
+if (!isValid(userName)) {
+    alert('No name entered, \n Good by!')
+}
+else nameCheck(userName, names);
+
 function nameCheck(name, dataBase) {
     var isCorrect = false;
     var length = dataBase.length;
@@ -19,5 +28,13 @@ function nameCheck(name, dataBase) {
             break;
         }
     }
-    if (!isCorrect) alert('No such username!')
+    if (!isCorrect) alert('No such username!');
+}
+function isValid(name) {
+    var result = true;
+    if(name.length < 1 || name === 'null') {
+        result= false;
+    } return result;
+
+
 }
