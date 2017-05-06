d6f3c3853e2362704e0c2500cd773646e7efc662
d_ryzhkov <rd.pochta@gmail.com>
2017-05-06 19:37:34 +0300

    js homework3-04.

--- js/homeWork03-4/js/script.js
+++ js/homeWork03-4/js/script.js
@@ -22,30 +22,30 @@ var test = {
     createTest: function () {
         var x = document.createElement('div');
         x.setAttribute('class', 'container-fluid');
-       document.body.appendChild(x);
-       var z = document.createElement('div');
-       z.setAttribute('class', 'page-header' );
-       x.appendChild(z);
-       var h = document.createElement('h3');
-       h.setAttribute('class', 'text-center');
-       var t = document.createTextNode(this.data.title);
-       h.appendChild(t);
-       z.appendChild(h);
-       var len = this.data.questions.length;
-       for (var i = 0; i < len; i++ ) {
-           var qt = document.createElement('p');
-           var tit = document.createTextNode(this.data.questions[i].title);
-           qt.appendChild(tit);
-           x.appendChild(qt);
-           var ans = this.data.questions[i].answers.length;
-           for(var m = 0; m <ans; ans++) {
-             var a = document.createElement('div');
-             a.setAttribute('class', 'checkbox');
-             var label = document.createElement('label');
-             a.appendChild(label);
-             x.appendChild(a);
-           }
-       }
+        document.body.appendChild(x);
+        var z = document.createElement('div');
+        z.setAttribute('class', 'page-header');
+        x.appendChild(z);
+        var h = document.createElement('h3');
+        h.setAttribute('class', 'text-center');
+        var t = document.createTextNode(this.data.title);
+        h.appendChild(t);
+        z.appendChild(h);
+        var len = this.data.questions.length;
+        for (var i = 0; i < len; i++) {
+            var qt = document.createElement('p');
+            var tit = document.createTextNode(this.data.questions[i].title);
+            qt.appendChild(tit);
+            x.appendChild(qt);
+            var ans = this.data.questions[i].answers.length;
+            // for (var m = 0; m < ans; ans++) {
+            //     var a = document.createElement('div');
+            //     a.setAttribute('class', 'checkbox');
+            //     var label = document.createElement('label');
+            //     a.appendChild(label);
+            //     x.appendChild(a);
+            // }
+        }
 
 
     }
