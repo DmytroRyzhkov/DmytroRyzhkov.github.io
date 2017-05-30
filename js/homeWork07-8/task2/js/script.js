dae0fa9e2670ef19f05e24bc0184697e868809a6
d_ryzhkov <rd.pochta@gmail.com>
2017-05-30 11:06:03 +0300

    js homework07-8 code refactoring done.

--- js/homeWork07-8/task2/js/script.js
+++ js/homeWork07-8/task2/js/script.js
@@ -3,5 +3,21 @@
  */
 'use strict';
 $(function () {
-
+    $('input').hover(function () {
+        var tooltip = this.title;
+        console.log(tooltip);
+        makeRed(this);
+    }, function () {
+        makeWhite(this);
+    });
+    $('button').on('click', function () {
+      var elem =  $('input');
+      makeRed(elem);
+    });
+    function makeRed(elem) {
+        $(elem).parent().css("background-color", "red");
+    }
+    function makeWhite(elem) {
+        $(elem).parent().css("background-color", "white");
+    }
 });
